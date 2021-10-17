dae097d93fc0e299d1e31ac4c724c6b1e8ba4b9e
qa-alam <alam.itprofessional@gmail.com>
2021-10-16 20:25:06 -0400

    Update

--- src/test/java/practice/collections/codes/ListClass.java
+++ src/test/java/practice/collections/codes/ListClass.java
@@ -14,8 +14,41 @@ import org.testng.annotations.Test;
 
 public class ListClass {
 
-	// List advantage
-
+	// What is the collection?
+	// Ans:- The collection in java a framework that an architecture to store and 
+	// manipulate the group of object 
+	
+	
+	// Differences between Collection, Collections & Collectors 
+	// Collection -> The Collection is an interface to store and manipulate the group of the object
+	
+	
+	// Collections -> Collections is a utility class in java, Help with with collections we can 
+	// reverse the string/number of value, find out max, min value as well as many more operations 
+	
+	
+	// Collectors -> Collectors is final class that extend the object class,Help 
+	// of collector we can remove the duplicate value & many more 
+	
+	// What is the List?
+	 // List is an interface
+	// List implements many class like (ArrayList, LinkedList, Vector & stack)
+	
+	// ArrayList advantage
+       // ArrayList support dynamic arrays that can grow as needed
+	   // ArrayList allows duplicate elements and null values as well
+	   // ArrayList class maintain the insertion order 
+	   // ArrayList is non-synchronized, That means it's faster 
+	   // ArrayList flowing the index theory java 
+	   // ArrayList when add the new values increases the size 50%
+	   // ArrayList faster then vector 
+	   // ArrayList slower then array
+	
+	
+	
+	
+	
+	
 	// Generic style
 	public static void main(String[] args) {
 
