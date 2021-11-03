44c0d906451e40668e3d0a1eb826008d7f5ee3e9
qa-alam <alam.itprofessional@gmail.com>
2021-11-02 22:57:47 -0400

    Alam

--- src/test/java/selenium/chalenges/StaleelementreferenceexceptionTest.java
+++ src/test/java/selenium/chalenges/StaleelementreferenceexceptionTest.java
@@ -8,6 +8,34 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class StaleelementreferenceexceptionTest {
+// What is the StaleElementReferenceException & how you can resolved?
+
+//When an element is no longer attached to the DOM, 
+//it has been removed from the document or the document has changed, 
+//it is said to be stale.
+
+	// Solution 1: Refreshing the web page
+	// You could refresh the page and try again for the same element.
+
+	// Solution 2: Using Try Catch Block
+	// If an element is not attached to DOM then you could try using
+	// ‘try-catch block’ within ‘for loop’
+
+	
+// Using for loop, it tries for 3 times. 
+// If the element is located for the first time then it breaks from the for 
+// loop nad comeout of the loop
+// for(int i=0; i<=2;i++){
+// try{
+// driver.findElement(By.xpath("xpath here")).click();
+//  break;}
+// catch(Exception e){
+// Sysout(e.getMessage()); } }
+
+	
+	// Solution 3: Using ExpectedConditions.refreshed
+	// Wait for the element till it gets available
+	// wait.until(ExpectedConditions.presenceOfElementLocated(By.id("table")));
 
 	public static void main(String[] args) {
 
